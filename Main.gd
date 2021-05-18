9a0771c2f67b3990dda7da2ceaf8a3e84ce9d2f2
holyslav <holy.slav36@gmail.com>
2021-05-18 10:56:09 +0300

    finish

--- Main.gd
+++ Main.gd
@@ -1,15 +1,32 @@
 extends Control
 
 onready var select_lang = $Content/Language/Select
+onready var content = $Content
+onready var loading = $Loading
+onready var tw = $Tween
 
 func _ready():
-	if not Boot.inited:
-		yield(Boot, "inited_updated")
+	content.modulate.a = 0 #установка прозрачности контейнера
+	
+	while not Boot.inited: #ожидание пока локали не прогрузятся
+		tw.interpolate_property(loading, "rect_rotation", 0, 360, 2, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT) #вращение логотипа загрузки
+		tw.start()
+		yield(tw, "tween_all_completed") #ожидание окончания анимации
 		
-	for locale in TranslationServer.get_loaded_locales():
+	show_page()
+	
+func show_page():
+	tw.interpolate_property(loading, "modulate:a", 1, 0, 1, Tween.TRANS_EXPO, Tween.EASE_IN_OUT) #скрытие через прозрачность
+	tw.interpolate_property(content, "modulate:a", 0, 1, 1, Tween.TRANS_EXPO, Tween.EASE_IN_OUT) #показ
+	tw.start()
+	
+	for locale in TranslationServer.get_loaded_locales(): #перебор загруженных локалей и добавление в опшинал
 		select_lang.add_item(locale.to_upper())
-		
+	
+	select_lang.select(TranslationServer.get_loaded_locales().find(TranslationServer.get_locale())) #установка текущей локали
+	
 	select_lang.connect("item_selected", self, "on_select_lang")
 	
+	
 func on_select_lang(idx):
-	TranslationServer.set_locale(TranslationServer.get_loaded_locales()[idx])
+	TranslationServer.set_locale(TranslationServer.get_loaded_locales()[idx]) #установка локали исходя из выбранного итема
--- boot.gd
+++ boot.gd
@@ -2,39 +2,35 @@ extends Node
 
 var http = null
 
-const git_path = "https://raw.githubusercontent.com/holyslav/RemoteLocalizeGodot_lang_files/main/%s.json"
+const git_path = "https://raw.githubusercontent.com/holyslav/RemoteLocalizeGodot_lang_files/main/%s.json" #путь к репозиторию где лежат ваши локали
 
 var inited = false
 
-signal inited_updated
+func get_json(filename:String): #Функция запроса к http серверу для получения содержимого файла
+	http.request(git_path % filename) #делаем реквест
+	var json_req = yield(http, "request_completed")  #ждём ответ
+	var json_body = json_req[3] #вытаскиваем боди
+	if json_body:
+		return JSON.parse(json_body.get_string_from_utf8()).result #парсим боди в жсон и отдаём
+	return null
 
 func _ready():
 	http = HTTPRequest.new()
 	add_child(http)
 	
-	http.request(git_path % "locales")
-	var locales_raw = yield(http, "request_completed")
-	var locales_body = locales_raw[3]
-	if locales_body:
-		
-		var locales_json = JSON.parse(locales_body.get_string_from_utf8()).result
-		
-		for locale in locales_json.locales:
-			
-			http.request(git_path % locale)
-			var locale_raw = yield(http, "request_completed")
-			var locale_body = locale_raw[3]
-			var locale_json = JSON.parse(locale_body.get_string_from_utf8()).result
-
-			var trans = Translation.new()
-			trans.set_locale(locale)
-			for word in locale_json:
-				trans.add_message(word, locale_json[word])
-
-			TranslationServer.add_translation(trans)
+	var locales_obj = yield(get_json("locales"), "completed") #получаем список локалей
+	if locales_obj:
+		for locale in locales_obj.locales:
+			 #запрашиваем каждую локаль
+			var locale_json = yield(get_json(locale), "completed")
+			if locale_json:
+				var trans = Translation.new() #создаём локаль для сервера переводов годо
+				trans.set_locale(locale) #установка названия локали
+				for word in locale_json: #перебор каждого слова и добавление в локаль при желании здесь можно сделать какую-то фильтрацию/модификацию переводов
+					trans.add_message(word, locale_json[word])
+				TranslationServer.add_translation(trans)
 			
 	var system_locale = OS.get_locale().split('_')[0]
 	TranslationServer.set_locale(system_locale)
 	
 	inited = true
-	emit_signal("inited_updated")
